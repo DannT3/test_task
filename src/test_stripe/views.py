9c8c8fa30426dc98a6cac11eaa3cac2999d018b9
Daniil Tishin <danya.tishin.2002@mail.ru>
2025-08-02 21:23:57 +0300

    create model Item

--- src/test_stripe/views.py
+++ src/test_stripe/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
     return HttpResponse("Hello World!")
+
+def show_item_info(request):
+    pass
+
 
 
 
--- src/testtask/settings.py
+++ src/testtask/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'test_stripe.apps.TestStripeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
